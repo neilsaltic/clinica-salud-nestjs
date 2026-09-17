aa1f745555ae44819e22ad3661802fc736ad4964
neilsaltic <neilsalanocaticona@gmail.com>
2026-09-17 17:38:27 -0400

    Filtros y excepciones

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { PrismaExceptionFilter } from './prisma/prisma-exception.filter.js';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+  app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
